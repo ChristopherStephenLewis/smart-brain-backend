c0f3fe0857ca9c5302a44da4a53168a8b01b02ec
Christopher Lewis <chris.stephen.lewis@gmail.com>
2023-08-20 16:15:09 -0400

    updated database file for heroku database

--- db/database.js
+++ db/database.js
@@ -3,13 +3,21 @@ const knex = require('knex');
 const db = knex ({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
-    user: 'postgres',
-    port: 5432,
-    password: 'Pheros12',
-    database: 'smart_brain'
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
   }
 });
+
+// const db = knex ({
+//   client: 'pg',
+//   connection: {
+//     host: '127.0.0.1',
+//     user: 'postgres',
+//     port: 5432,
+//     password: 'Pheros12',
+//     database: 'smart_brain'
+//   }
+// });
 
 //enable to console log all entries in the users database
 // db.select('*').from('users').then(data => {
