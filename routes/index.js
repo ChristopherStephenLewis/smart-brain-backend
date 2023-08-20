44a193b84abdab6a07b60e58e690b15ef4b4c069
Christopher Lewis <chris.stephen.lewis@gmail.com>
2023-08-20 16:44:28 -0400

    Further testing

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // GET route for the main index page
 router.get('/', (req, res) => {
-  res.send('Index');
+  res.send('Index!');
 });
 
 module.exports = router;
